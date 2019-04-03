0ff1bb45f40ee6c52b90447a2cba04dde33c3e9d
= <=>
2019-04-03 06:28:23 +0530

    database added and blog fn for user done

--- backend/controller.js
+++ backend/controller.js
@@ -1,10 +1,17 @@
+var model = require('./model.js');
 module.exports = {
 	usermap : function(req,res){
-		res.send("map")
+		res.send(model.place())	//return the data of different places
 	},
 	
 	blog : function(req,res){
-		res.send("blog")
+		var filename  = "/home/snowman/Desktop/GROUP_F_PROJECT-13/backend/blog/" + req.query.lat + "_" + req.query.lon + ".html";
+		res.sendFile(filename,{dotfiles: 'deny'},function(err){
+			if(err)
+				res.send("no such blog")
+			else
+				console.log("sent")
+		})
 	},
 
 	adminlogin : function(req, res){
